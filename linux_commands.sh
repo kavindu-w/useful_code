42abb56637ebbaebfc231b3dc0c55464c55defb6
Kavindu Warnakulasuriya <akwarnakulasuriya@gmail.com>
2023-07-24 14:50:47 +0530

    rpal batch commands

--- linux_commands.sh
+++ linux_commands.sh
@@ -27,3 +27,23 @@ sudo chmod 774 mr.sh
 	
 # // open directory in vscode
 code .
+
+
+# run programs and output them to files(RPAL) -> go to directory
+for file in *; do
+    if [[ -f "$file" && "$file" != "rpal" && "$file" != "rpal.exe" && "$file" != "cygwin1.dll" ]]; then
+        ./rpal "./$file" > "${file}_out"
+    fi
+done
+# go through the files and echo the commands to append to makefile java
+#!/bin/bash
+
+# Loop through files in the folder
+for file in *; do
+    # Check if it's a regular file and not one of the excluded files
+    if [[ -f "$file" && "$file" != "rpal" && "$file" != "rpal.exe" && "$file" != "cygwin1.dll" && "$file" != *_out ]]; then
+        echo "java -cp bin rpal20 tests_v2/$file > output.01"
+        echo "fc output.01 tests_v2/${file}_out"
+        echo
+    fi
+done
